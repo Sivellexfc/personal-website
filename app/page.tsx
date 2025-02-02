d987e68aa3b59f961db229305043e452d1bf9f17
Musluhan Çavuş <musluhan01@hotmail.com>
2025-02-02 21:51:30 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,299 @@
+"use client";
+
 import Image from "next/image";
+import pp from "../public/pp.jpg";
+import ThemeToggle from "../components/ui/theme-toggle";
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Card, CardContent } from "@/components/ui/card";
+
+import im1 from "../public/1.png";
+import im2 from "../public/2.jpg";
+import im3 from "../public/3.png";
+import im4 from "../public/4.png";
+import im5 from "../public/5.png";
+import im6 from "../public/6.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
+      <div className="mx-auto max-w-xl px-4 py-20">
+        <motion.header
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          className="flex items-center justify-between mb-12"
+        >
+          <div className="w-20 h-20 rounded-full overflow-hidden">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+              src={pp}
+              alt="profile-picture"
+              className="cursor-pointer transition-all duration-300 hover:scale-110 "
+            ></Image>
+          </div>
+          <ThemeToggle></ThemeToggle>
+        </motion.header>
+
+        <main className="space-y-10">
+          <section className="space-y-10">
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+              className="space-y-1"
+            >
+              <h1 className="text-4xl font-bold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent">
+                Musluhan Çavuş
+              </h1>
+              <p className="text-gray-600 dark:text-gray-400">
+                Computer Engineer & Software Developer
+              </p>
+              <p className="text-gray-600 dark:text-gray-400">
+                Bursa,Izmir,Denizli
+              </p>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+              className="space-y-3"
+            >
+              <h2 className="text-lg font-semibold">
+                Buildin Scalable Solutiouns for tomorrow
+              </h2>
+              <p className="text-gray-600 text-sm dark:text-gray-400 max-w-2xl">
+                Expert in cloud architecture with{" "}
+                <span className="text-black dark:text-white">AWS </span>and{" "}
+                <span className="text-black dark:text-white">Azure</span>. I
+                specialize in designing and implementing lareg-scale distributed
+                systems. With deep expertise in{" "}
+                <span className="text-black dark:text-white">
+                  Python, Kubernates and microservice architecture
+                </span>
+                .
+              </p>
+              <p className="text-gray-600 text-sm dark:text-gray-400 max-w-2xl">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+              className="flex items-center gap-3"
+            >
+              <Button className="rounded-full bg-gradient-to-r from-rose-600 to-indigo-600 text-white transition-transform hover:scale-105">
+                Resume
+              </Button>
+              <Link
+                href="#"
+                className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors"
+              >
+                <FaGithub className="w-6 h-6"></FaGithub>
+              </Link>
+
+              <Link
+                href="#"
+                className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors"
+              >
+                <FaLinkedin className="w-6 h-6"></FaLinkedin>
+              </Link>
+            </motion.div>
+          </section>
+
+          <motion.section
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="space-y-8"
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">
+              Experience
+            </h2>
+
+            <div className="space-y-8">
+              <div className="flex items-center justify-between">
+                <div className="space-y-1">
+                  <p className="text-sm text-gray-600 dark:text-gray-400 ">
+                    July, 2024 - Present
+                  </p>
+                  <p className="font-semibold">System Engineer</p>
+                  <p className="text-blue-600 dark:text-blue-400">Company X</p>
+                </div>
+              </div>
+              <p className="text-gray-600 dark:text-gray-400 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's.
+              </p>
+            </div>
+
+            <div className="space-y-8">
+              <div className="flex items-center justify-between">
+                <div className="space-y-1">
+                  <p className="text-sm text-gray-600 dark:text-gray-400 ">
+                    July, 2024 - Present
+                  </p>
+                  <p className="font-semibold">System Engineer</p>
+                  <p className="text-blue-600 dark:text-blue-400">Company X</p>
+                </div>
+              </div>
+              <p className="text-gray-600 dark:text-gray-400 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's.
+              </p>
+            </div>
+
+            <div className="space-y-8">
+              <div className="flex items-center justify-between">
+                <div className="space-y-1">
+                  <p className="text-sm text-gray-600 dark:text-gray-400 ">
+                    July, 2024 - Present
+                  </p>
+                  <p className="font-semibold">System Engineer</p>
+                  <p className="text-blue-600 dark:text-blue-400">Company X</p>
+                </div>
+              </div>
+              <p className="text-gray-600 dark:text-gray-400 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's.
+              </p>
+            </div>
+          </motion.section>
+
+          <motion.section
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="space-y-8"
+          >
+            <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">
+              Education
+            </h2>
+
+            <div className="space-y-5">
+              <div className="flex items-center justify-between">
+                <div className="space-y-1">
+                  <p className="text-sm text-gray-600 dark:text-gray-400 ">
+                    July, 2024 - Present
+                  </p>
+                  <p className="font-semibold">Computer Engineer</p>
+                  <p className="text-blue-600 dark:text-blue-400 text-sm">
+                    Porto University
+                  </p>
+                </div>
+              </div>
+              <p className="text-gray-600 dark:text-gray-400 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown.
+              </p>
+            </div>
+          </motion.section>
+
+          <motion.section
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="space-y-8"
+          >
+            <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">
+              Projects
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+              <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                <CardContent className="p-4">
+                  <Image src={im1} alt="" className="rounded-lg mb-4 ">
+                  </Image>
+                  <div className="flex items-center justify-between">
+                      <div>
+                        <h3 className="font-medium">Project 1</h3>
+                        <p className="text-sm text-gray-600 dark:text-gray-400">Next.js MySQL</p>
+                      </div>
+                      <Button variant="ghost" size="icon"></Button>
+                    </div>
+                </CardContent>
+              </Card>
+
+              <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                <CardContent className="p-4">
+                  <Image src={im2} alt="" className="rounded-lg mb-4 ">
+                  </Image>
+                  <div className="flex items-center justify-between">
+                      <div>
+                        <h3 className="font-medium">Project 1</h3>
+                        <p className="text-sm text-gray-600 dark:text-gray-400">Next.js MySQL</p>
+                      </div>
+                      <Button variant="ghost" size="icon"></Button>
+                    </div>
+                </CardContent>
+              </Card>
+
+              <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                <CardContent className="p-4">
+                  <Image src={im3} alt="" className="rounded-lg mb-4 ">
+                  </Image>
+                  <div className="flex items-center justify-between">
+                      <div>
+                        <h3 className="font-medium">Project 1</h3>
+                        <p className="text-sm text-gray-600 dark:text-gray-400">Next.js MySQL</p>
+                      </div>
+                      <Button variant="ghost" size="icon"></Button>
+                    </div>
+                </CardContent>
+              </Card>
+
+              <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                <CardContent className="p-4">
+                  <Image src={im4} alt="" className="rounded-lg mb-4 ">
+                  </Image>
+                  <div className="flex items-center justify-between">
+                      <div>
+                        <h3 className="font-medium">Project 1</h3>
+                        <p className="text-sm text-gray-600 dark:text-gray-400">Next.js MySQL</p>
+                      </div>
+                      <Button variant="ghost" size="icon"></Button>
+                    </div>
+                </CardContent>
+              </Card>
+
+              <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                <CardContent className="p-4">
+                  <Image src={im5} alt="" className="rounded-lg mb-4 ">
+                  </Image>
+                  <div className="flex items-center justify-between">
+                      <div>
+                        <h3 className="font-medium">Project 1</h3>
+                        <p className="text-sm text-gray-600 dark:text-gray-400">Next.js MySQL</p>
+                      </div>
+                      <Button variant="ghost" size="icon"></Button>
+                    </div>
+                </CardContent>
+              </Card>
+            </div>
+            <p className="text-gray-600">© 2025 Musluhan Çavuş</p>
+          </motion.section>
+          
+        </main>
+      </div>
     </div>
   );
 }
