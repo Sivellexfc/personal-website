47c7b62835332a78f2b76a689a38830eed85d8fc
Musluhan Çavuş <musluhan01@hotmail.com>
2025-02-02 23:16:06 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -129,7 +129,7 @@ export default function Home() {
               </div>
               <p className="text-gray-600 dark:text-gray-400 text-sm">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
+                industry. Lorem Ipsum has been the industrys standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book. Lorem
                 Ipsum is simply dummy text of the printing and typesetting
@@ -149,7 +149,7 @@ export default function Home() {
               </div>
               <p className="text-gray-600 dark:text-gray-400 text-sm">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
+                industry. Lorem Ipsum has been the industrys standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book. Lorem
                 Ipsum is simply dummy text of the printing and typesetting
@@ -169,7 +169,7 @@ export default function Home() {
               </div>
               <p className="text-gray-600 dark:text-gray-400 text-sm">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
+                industry. Lorem Ipsum has been the industrys standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book. Lorem
                 Ipsum is simply dummy text of the printing and typesetting
