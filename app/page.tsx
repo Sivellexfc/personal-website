a6384a7e1ce7123ec04e859fe62e238796ac6bf2
Musluhan Çavuş <musluhan01@hotmail.com>
2025-02-06 00:27:55 +0300

    added personal infos

--- app/page.tsx
+++ app/page.tsx
@@ -25,13 +25,13 @@ export default function Home() {
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8 }}
-          className="flex items-center justify-evenly mb-12 p-1 border border-gray-800 dark:border-gray-200 dark:border-opacity-20 border-opacity-20 rounded-xl"
+          className="sticky top-[+30] left-0 w-full z-50 flex items-center justify-evenly mb-12 p-1 border bg-white dark:bg-black border-gray-800 dark:border-gray-200 dark:border-opacity-20 border-opacity-20 rounded-xl transition-all duration-300"
         >
           <ScrollLink
             to="aboutme"
             smooth={true}
             duration={500}
-            offset={-300}
+            offset={-100}
             className="cursor-pointer"
           >
             About Me
@@ -40,7 +40,7 @@ export default function Home() {
             to="skills"
             smooth={true}
             duration={500}
-            offset={-300}
+            offset={-100}
             className="cursor-pointer"
           >
             Skills
@@ -49,7 +49,7 @@ export default function Home() {
             to="education"
             smooth={true}
             duration={500}
-            offset={-300}
+            offset={-100}
             className="cursor-pointer"
           >
             Education
@@ -58,7 +58,7 @@ export default function Home() {
             to="projects"
             smooth={true}
             duration={500}
-            offset={-300}
+            offset={-100}
             className="cursor-pointer"
           >
             Projects
@@ -86,14 +86,14 @@ export default function Home() {
                       ></Image>
                     </div>
                     <div>
-                      <h1 className="text-4xl font-bold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent">
+                      <h1 className="text-4xl font-bold bg-gradient-to-r from-rose-600 via-indigo-500 to-purple-400 bg-clip-text text-transparent">
                         Musluhan Çavuş
                       </h1>
                       <p className="text-gray-600 dark:text-gray-400">
                         Computer Engineer & Software Developer
                       </p>
                       <p className="text-gray-600 dark:text-gray-400">
-                        Bursa,Izmir,Denizli
+                        Izmir, Denizli, Turkey
                       </p>
                       <motion.div
                         initial={{ opacity: 0, x: 50 }}
@@ -101,7 +101,7 @@ export default function Home() {
                         transition={{ duration: 0.8 }}
                         className="flex items-center gap-3 mt-6"
                       >
-                        <Button className="rounded-xl bg-gradient-to-r from-rose-600 to-indigo-600 text-white transition-transform hover:scale-105">
+                        <Button className="rounded-xl bg-gradient-to-r from-rose-600 to-indigo-300 text-white transition-transform hover:scale-105">
                           Resume
                           <IoDownloadOutline></IoDownloadOutline>
                         </Button>
@@ -132,24 +132,34 @@ export default function Home() {
               className="space-y-3"
             >
               <h2 className="text-lg font-semibold">
-                Buildin Scalable Solutiouns for tomorrow
+                Kullanıcı dostu, yenilikçi web çözümleri
               </h2>
+
               <p className="text-gray-600 text-sm dark:text-gray-400 max-w-2xl">
-                Expert in cloud architecture with{" "}
-                <span className="text-black dark:text-white">AWS </span>and{" "}
-                <span className="text-black dark:text-white">Azure</span>. I
-                specialize in designing and implementing lareg-scale distributed
-                systems. With deep expertise in{" "}
                 <span className="text-black dark:text-white">
-                  Python, Kubernates and microservice architecture
+                  React, Nextjs{" "}
                 </span>
-                .
+                frameworklerinde deneyim ve bilgi sahibi
+                <span className="text-black dark:text-white">
+                  {" "}
+                  Frontend Developer
+                </span>
+                , Huawei Cloud HCCDA – Tech Essentials Sertifkalı Cloud
+                geliştiricisi.{" "}
               </p>
               <p className="text-gray-600 text-sm dark:text-gray-400 max-w-2xl">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.
+                Merhaba, ben Musluhan, Pamukkale Üniversitesi 4. sınıf
+                bilgisayar mühendisliği öğrencisiyim. Web geliştirme alanında
+                React, HTML, CSS, Tailwind gibi teknolojilerde derinlemesine
+                bilgiye sahipken, Next.js ve Vue gibi modern frameworklerde
+                çalışmalar yapıyorum. Uygulama geliştirme sürecinde, kullanıcı
+                dostu ve etkili çözümler üretmek için en iyi uygulamaları ve
+                güncel web teknolojilerini kullanmaya özen gösteriyorum. Ayrıca
+                DevOps alanında kendimi geliştiriyor ve cloud
+                teknolojileri,container teknolojileri, Kubernetes ile monitoring
+                araçları üzerinde deneyim kazanıyorum. Bu alandaki bilgi
+                birikimimi sürekli olarak genişletiyor ve yenilikçi çözümler
+                üretmeye odaklanıyorum.
               </p>
             </motion.div>
           </section>
@@ -161,7 +171,7 @@ export default function Home() {
               transition={{ duration: 0.8 }}
               className="space-y-8"
             >
-              <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">
+              <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-purple-400 bg-clip-text text-transparent inline-block">
                 Experience
               </h2>
 
@@ -247,22 +257,58 @@ export default function Home() {
                 Education
               </h2>
 
-              <div className="space-y-5">
+              <div className="space-y-2">
                 <div className="flex items-center justify-between">
                   <div className="space-y-1">
                     <p className="text-sm text-gray-600 dark:text-gray-400 ">
-                      July, 2024 - Present
+                      2021 - Present
                     </p>
-                    <p className="font-semibold">Computer Engineer</p>
+                    <p className="font-semibold">Computer Engineering</p>
                     <p className="text-blue-600 dark:text-blue-400 text-sm">
-                      Porto University
+                      Pamukkale University
                     </p>
                   </div>
                 </div>
                 <p className="text-gray-600 dark:text-gray-400 text-sm">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industrys
-                  standard dummy text ever since the 1500s, when an unknown.
+                  I am currently continuing my studies as a fourth-year student
+                  in the Computer Engineering department, which I enrolled in
+                  2021.
+                </p>
+              </div>
+
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <div className="space-y-1">
+                    <p className="text-sm text-gray-600 dark:text-gray-400 ">
+                      2024 FEB - JUNE
+                    </p>
+                    <p className="font-semibold">Computer Sience Engineering</p>
+                    <p className="text-blue-600 dark:text-blue-400 text-sm">
+                      Polytechnic Institute of Porto
+                    </p>
+                  </div>
+                </div>
+                <p className="text-gray-600 dark:text-gray-400 text-sm">
+                  I studied at the Polytechnic Institute of Porto in Portugal as
+                  part of the Erasmus+ Project during the Spring semester of the
+                  2023-2024 academic year.
+                </p>
+              </div>
+
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <div className="space-y-1">
+                    <p className="text-sm text-gray-600 dark:text-gray-400 ">
+                      2015 - 2019
+                    </p>
+                    <p className="text-blue-600 dark:text-blue-400 text-sm">
+                      Bursa Cumhuriyet Anatolian High School
+                    </p>
+                  </div>
+                </div>
+                <p className="text-gray-600 dark:text-gray-400 text-sm">
+                  In 2015, I gained admission to Bursa Cumhuriyet Anatolian High
+                  School and successfully completed my studies in 2019.
                 </p>
               </div>
             </motion.section>
@@ -376,7 +422,9 @@ export default function Home() {
               </div>
             </motion.section>
           </Element>
-          <p className="text-gray-600 justify-center flex">© 2025 Musluhan Çavuş</p>
+          <p className="text-gray-600 justify-center flex">
+            © 2025 Musluhan Çavuş
+          </p>
         </main>
       </div>
     </div>
---
+++ components/ui/header.tsx
@@ -0,0 +1,55 @@
+import { motion } from "framer-motion";
+import { Link as ScrollLink } from "react-scroll";
+import ThemeToggle from "./theme-toggle";
+
+const Header = () => {
+  return (
+    <motion.header
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.8 }}
+      className="backdrop-blur-sm bg-background/55 sticky top-0 w-full z-50 flex items-center justify-evenly mb-12 p-1 border border-gray-800 dark:border-gray-200 dark:border-opacity-20 border-opacity-20 rounded-xl"
+    >
+      <ScrollLink
+        to="aboutme"
+        smooth={true}
+        duration={500}
+        offset={-100}
+        className="cursor-pointer"
+      >
+        About Me
+      </ScrollLink>
+      <ScrollLink
+        to="skills"
+        smooth={true}
+        duration={500}
+        offset={-100}
+        className="cursor-pointer"
+      >
+        Skills
+      </ScrollLink>
+      <ScrollLink
+        to="education"
+        smooth={true}
+        duration={500}
+        offset={-100}
+        className="cursor-pointer"
+      >
+        Education
+      </ScrollLink>
+      <ScrollLink
+        to="projects"
+        smooth={true}
+        duration={500}
+        offset={-100}
+        className="cursor-pointer"
+      >
+        Projects
+      </ScrollLink>
+
+      <ThemeToggle></ThemeToggle>
+    </motion.header>
+  );
+};
+
+export default Header;
